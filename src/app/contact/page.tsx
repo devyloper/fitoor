9884ca14f895e5fa410025b4f4c751ec21e98404
Yuvraj Singh Raghuwanshi <y.s.raghuwanshi03@gmail.com>
2023-07-22 21:44:59 +0530

    features added
    Signed-off-by: Yuvraj Singh Raghuwanshi <y.s.raghuwanshi03@gmail.com>

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
-
+import Navbar from "@/app/components/Navbar";
+import Footer from "@/app/components/Footer";
+import Link from 'next/link'
 type Props = {}
 
 const page = (props: Props) => {
   return (
-    <div>contact</div>
+      <div>
+    <Navbar />
+      <div className="m-8 font-sans  text-[10px] flex justify-center">
+        <Link href={'/'} className="hover:underline">HOME</Link>&nbsp;/ CONTACT US
+      </div>
+      <div className="flex flex-col justify-between my-14 mx-auto h-[68vh] w-[48vw]">
+        <h1 className={`text-6xl font-bold font-pinyon`}>Contact us</h1>
+        <div className={`flex font-bold`}>Head Office Address</div>
+        <div className={`flex`}>Shop No. 20 & 21, Gurudwara Tekri Sahib, Idgah Hills, Bhopal, M.P. 462001</div>
+        <div className={`flex font-bold`}>Mobile No.</div>
+        <div className={`flex`}>+91 9755331021</div>
+        <div className={`flex font-bold`}>Email Address</div>
+        <div className={`flex`}>support@klassy.in</div>
+        <div className={`flex font-bold`}>Working Hours</div>
+        <div className={`flex`}>Mon-Sat (11 am to 8 pm)</div>
+      </div>
+    <Footer />
+      </div>
   )
 }
+
 
 export default page
